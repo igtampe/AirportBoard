2a45095b7185dfff3151aa0c24ae2b5a8011abe4
Ignacio Tampe <Igtampe@outlook.com>
2020-05-20 19:37:57 -0400

    Test Commit, Future WeatherPage

---
+++ LandingPad/LandingPageElements/WeatherPage.vb
@@ -0,0 +1,6 @@
+﻿Public Class WeatherPage
+
+    'This will eventually use the NOAA information to display actual weather, given a locaiton.
+    'Hopefully. Maybe. This is mostly here for a test commit.
+
+End Class
