7942dd287a1596f1ba9b2285feeb3285439b400b
Ignacio Tampe <Igtampe@outlook.com>
2020-05-20 19:38:58 -0400

    One more test commit

--- LandingPad/LandingPageElements/WeatherPage.vb
+++ LandingPad/LandingPageElements/WeatherPage.vb
@@ -3,4 +3,6 @@
     'This will eventually use the NOAA information to display actual weather, given a locaiton.
     'Hopefully. Maybe. This is mostly here for a test commit.
 
+    'Just one more test commit
+
 End Class
